fc95462e0fe29445717ea21f1b28032ccc16980e
Daniel Sebastian G. Quilatan <quilatandg@students.nu-moa.edu.ph>
2024-02-25 16:16:06 +0800

    asd

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
+      <Text>Open up App.js to starddsdsdsft wodsdsrkidsdng on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#913939',
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
+  
 });
